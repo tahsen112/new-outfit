07efdb6d003133ec53d258ef2949894e6799a6a9
Tahsen <tahsen.2006.198@gmail.com>
2024-12-07 17:03:59 +0300

    updates

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
 <a href="#about-us">about us</a> */
 
 let nav = document.querySelector(`nav`);
+
+if (screen.width <= 450) {
+     console.log(nav.innerHTML);
+     nav.innerHTML = `<i class="fa-solid fa-bars" onclick="getNav()" ></i>`;
+}
+
 window.addEventListener(`load`, () => {
-     if (screen.width <= 450) {
-          console.log(nav.innerHTML);
-          nav.innerHTML = `<i class="fa-solid fa-bars" onclick="getNav()" ></i>`;
-     }
+     document.querySelector(`.container`).style.display = `block`;
 });
 
 function getNav() {
